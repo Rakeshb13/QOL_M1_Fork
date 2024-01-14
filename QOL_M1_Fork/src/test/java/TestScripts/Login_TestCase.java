4d2d581732a579c82e8efe555e434ead5c72048d
smurt <smurt@DESKTOP-QARHQMJ>
2024-01-14 12:53:31 +0530

    Changes made,please check

--- QOL_M1_Fork/src/test/java/TestScripts/Login_TestCase.java
+++ QOL_M1_Fork/src/test/java/TestScripts/Login_TestCase.java
@@ -1,5 +1,22 @@
 package TestScripts;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Login_TestCase {
 
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	driver.get("https://demowebshop.tricentis.com/");
+	
+	driver.findElement(By.linkText("Log in")).click();
+	driver.findElement(By.id("Email")).sendKeys("smrutimayeepanda243@gmail.com");
+	driver.findElement(By.id("Password")).sendKeys("smruti@123");
+	driver.findElement(By.cssSelector("[value='Log in']")).click();
+			
+}
 }
